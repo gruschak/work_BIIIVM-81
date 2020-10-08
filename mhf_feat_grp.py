27ff8edf10b6a3d9ff635e056322eb832919480c
Maxim Grushchak <m.grushchak@bars-group.ru>
2020-10-08 18:09:27 +0300

    tried with pandas dataframes

--- mhf_feat_grp.py
+++ mhf_feat_grp.py
@@ -1,189 +1,151 @@
-import os
-import abc
-from typing import List, Optional, Dict
-from pandas import read_json
-from interfaces import IModel, IModelRepo, IModelPreserver, IModelIdentifier
-from datetime import datetime
+from typing import List, Optional, Dict, TypedDict
+import pandas as pd
+import csv
 
-DIR_MISS_HA_FEAT_MODELS = ""
-F_ASSOC_RULES = "assoc_rules.json"
+FEATURES_INPUT = ['other_diagnoses__ischemic_heart_disease', 'kbs__characteristic__compressive', 'kbs__characteristic__fully_relief', 'kbs__relief__medicines', 'other_diagnoses__myocardial_infarction', 'bad_habits__alcohol', 'other_symptoms__general_weakness', 'blood_pressure__during_at_checkup', 'blood_pressure__during_at_lifetime', 'kbs__characteristic__occurs_at_physical_activity', 'other_symptoms__dyspnea', 'other_diagnoses__hypertension']
+HEART_ATTACK_FEATURES = None
 
 
-def convert_to_set(value):
-    """"
-    """
-    try:
-        return set(value)
-    except ValueError:
-        return value
+class HeartAttackFeature(object):
+    def __init__(self):
+        self.id = None
+        self.group_code = None
+        self.code = None
+        self.name = None
 
 
-class StoredModelNotFound(Exception):
-    """
-    """
+class HeartAttackFeatures(object):
+    """ Признаки для heart_attack """
+    def __init__(self):
+        self._features = []
+
+    @property
+    def features(self) -> List[HeartAttackFeature]:
+        """ """
+        return self._features
 
 
-class ModelBase(IModel, metaclass=abc.ABCMeta):
-    """
-    Базовый класс модели. Определяются getter и setter для атрибута model_id.
-    """
+def load_heart_attack_features(path: str) -> HeartAttackFeatures:
+    """ """
+    result = HeartAttackFeatures()
+
+    with open(path, "rt") as f:
+        reader = csv.reader(f, delimiter=",")
+        next(reader, None)  # скипаем первую запись
+        for row in reader:
+            feature = HeartAttackFeature()
+            feature.id = int(row[0])
+            feature.group_code = int(row[1])
+            feature.code = row[2]
+            feature.name = row[3]
+            result.features.append(feature)
+
+    return result
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Группы признаков
+# ----------------------------------------------------------------------------------------------------------------------
+class HeartAttackFeatureGroup(object):
+    """ Группа признаков """
 
     def __init__(self):
-        self._model_id = None
-
-    @property
-    def model_id(self) -> IModelIdentifier:
-        return self._model_id
-
-    @model_id.setter
-    def model_id(self, value: IModelIdentifier):
-        self._model_id = value
+        self.id = None
+        self.code = None
+        self.name = None
 
 
-class MissedHAFeaturesModelIdentifier(IModelIdentifier):
-    """
-    Идентификатор модели поиска ассоциированных признаков. Определяется инициализация атрибута model_type
-    """
-    def __init__(self, model_type: str):
-        self._model_type = model_type
-
-    @property
-    def model_type(self) -> str:
-        return self._model_type
-
-
-class MissedHAFeaturesModelRepo(IModelRepo):
-
-    def get_model(self, model_id: MissedHAFeaturesModelIdentifier) -> IModel:
-        result = MissedHAFeaturesModel()
-
-        if result is not None:
-            result.model_id = model_id
-
-        return result
-
-    def get_preserver(self, model_id: MissedHAFeaturesModelIdentifier, **options) -> IModelPreserver:
-        return MissedHAFeaturesModelPreserver(**options)
-
-
-class MissedHAFeaturesModel(ModelBase):
+class HeartAttackFeatureGroups(object):
+    """ Список групп признаков """
     def __init__(self):
-        super().__init__()
-        self._assoc_rules = None  # data will be loaded from json-file as a Pandas Dataframe
+        self._groups = []
 
     @property
-    def assoc_rules(self):
-        return self._assoc_rules
-
-    @assoc_rules.setter
-    def assoc_rules(self, value):
-        self._assoc_rules = value
-
-    def fit(self, X, y, **fit_options):
-        raise NotImplementedError
-
-    def predict(self, features, **apply_rules_options):
-
-        # filter_series = self.assoc_rules['antecedents'] == set(features)
-
-        t_start = datetime.now()
-        aggregated_consequents = set()
-        for i in range(self.assoc_rules.shape[0]):
-            rule = self.assoc_rules.iloc[i]
-            if rule['antecedents'].issubset(features):
-                aggregated_consequents.update(rule['consequents'])
-        aggregated_consequents.difference_update(features)
-        t_finish = datetime.now()
-        print((t_finish - t_start).seconds)
-
-        return list(aggregated_consequents)
+    def groups(self) -> List[HeartAttackFeatureGroup]:
+        """ """
+        return self._groups
 
 
-class MissedHAFeaturesModelPreserver(IModelPreserver):
+def load_heart_attack_feature_groups(path: str) -> HeartAttackFeatureGroups:
+    """ """
+    result = HeartAttackFeatureGroups()
 
-    def __init__(self, storage_path: str):
-        self._storage_path = storage_path
+    with open(path, "rt") as f:
+        reader = csv.reader(f, delimiter=",")
+        next(reader, None)  # скипаем первую запись
+        for row in reader:
+            group = HeartAttackFeatureGroup()
+            group.id = int(row[0])
+            group.code = row[1]
+            group.name = row[2]
+            result.groups.append(group)
 
-    @property
-    def storage_path(self) -> str:
-        return self._storage_path
+    return result
 
-    def load(self, model: MissedHAFeaturesModel) -> MissedHAFeaturesModel:
-        if model.model_id.model_type == 'Apriori':
-            model.assoc_rules = read_json(os.path.join(self.storage_path, 'apriori.json'))
-        elif model.model_id.model_type == 'FPGrowth':
-            model.assoc_rules = read_json(os.path.join(self.storage_path, 'fpgrowth.json'))
-        model.assoc_rules.loc[:, 'antecedents'] = model.assoc_rules['antecedents'].apply(convert_to_set)
-        model.assoc_rules.loc[:, 'consequents'] = model.assoc_rules['consequents'].apply(convert_to_set)
-
-        return model
-
-    def dump(self, model: MissedHAFeaturesModel) -> MissedHAFeaturesModel:
-        raise NotImplementedError
+##################################################################################
 
 
-class MissedHAFeaturesModelsCache(object):
+def get_heart_attack_features() -> List[HeartAttackFeature]:
+    """ """
+    global HEART_ATTACK_FEATURES
+
+    if HEART_ATTACK_FEATURES is None:
+        path = "feature.csv"
+        HEART_ATTACK_FEATURES = load_heart_attack_features(path=path)
+
+    return HEART_ATTACK_FEATURES
+
+
+class HeartAttackFeaturesGroupPack(object):
+
     __shared_state = {
-        "_repo_cached": None,
-        "_model_cache": {}
+        "_groups": None
     }
 
     def __init__(self):
         self.__dict__ = self.__shared_state
 
     @property
-    def model_cache(self) -> Dict[str, Dict[str, MissedHAFeaturesModel]]:
-        return self._model_cache
-
-    @property
-    def repo_cached(self) -> MissedHAFeaturesModelRepo:
-        """ """
-        if self._repo_cached is None:
-            self._repo_cached = MissedHAFeaturesModelRepo()
-        return self._repo_cached
-
-    def get_model(self, model_type: str) -> Optional[IModel]:
-
-        if model_type not in self.model_cache:
-            self.model_cache[model_type] = {}
-
-            model_id = MissedHAFeaturesModelIdentifier(model_type=model_type)
-
-            model = self.repo_cached.get_model(model_id)
-            if model is not None:
-                preserver = self.repo_cached.get_preserver(
-                    model_id=model_id,
-                    # storage_path=get_env_value("PREDICT_HA_MODELS", None) or DIR_MISS_HA_FEAT_MODELS
-                    storage_path=DIR_MISS_HA_FEAT_MODELS
-                )
-                try:
-                    model = preserver.load(model)
-                except StoredModelNotFound:
-                    model = None
-                    pass
-
-            self.model_cache[model_type] = model
-
-        return self.model_cache[model_type]
+    def groups(self) -> List[HeartAttackFeatureGroup]:
+        return self._groups
 
 
-def missed_ha_features_by_features(features: List[str]):
-    result = None
+def get_heart_attack_feature_groups() -> List[HeartAttackFeatureGroup]:
 
-    model_cache = MissedHAFeaturesModelsCache()
+    pack = HeartAttackFeaturesGroupPack()
 
-    model = model_cache.get_model(model_type="FPGrowth")
+    if pack.groups is None:
+        # пытаемся загрузить группы
+        path = "feature_group.csv"
+        pack._groups = load_heart_attack_feature_groups(path=path)
 
-    if model:
-        result = model.predict(features)
-
-    return result
-
-
-# ________________________________________________________
+    return pack.groups
 
 
 if __name__ == '__main__':
-    input_features = ["other_symptoms__sensorimotor_aphasia", "bad_habits__smoking", "kbs__localization__sternum"]
-    res = missed_ha_features_by_features(input_features)
-    print(res)
+
+    input_features = FEATURES_INPUT
+
+    """
+    features = load_heart_attack_features("feature.csv")
+    groups = load_heart_attack_feature_groups("feature_group.csv")
+
+    for f in features.features:
+        print(f.name, f.id, f.code, f.group_code)
+
+    for g in groups.groups:
+        print(g.name, g.id, g.code)
+    """
+
+    FeaturesDF = pd.read_csv("feature.csv", delimiter=",")
+    FeatureGroupsDF = pd.read_csv("feature_group.csv", delimiter=",")
+    # print(DfFeatures[['id', 'code', 'name', 'feature_group_id']])
+    # print(FeatureGroupsDF[['id', 'code', 'name']])
+    # print(DfFeatures[DfFeatures['code'] == input_features[0]])
+    # for f in DfFeatures.values: print(f)
+    for f in input_features:
+        Feature = FeaturesDF[FeaturesDF['code'] == f]
+        FeatureGroup = FeatureGroupsDF[FeatureGroupsDF['id'] == int(Feature.feature_group_id)]
+        print(f" \"name\": \"{FeatureGroup['name'].values[0]} : {Feature.name.values[0]}\"")
+        print("----------------------------")
+
